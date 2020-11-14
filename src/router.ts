d28e66d06cc851326b53006f02b69f340e782ed3
Dave Welsh <dave@qccareerschool.com>
2020-11-14 16:47:57 -0500

    Handle empty depositOverride object

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,7 @@ import { pool } from './pool';
 import { oldGetPrices, OldPriceQuery, OldPriceResult } from './old-prices';
 
 const objectMap = (obj: any, mapFunction: (value: any) => any) => {
+  if (!obj) return {};
   return Object.keys(obj).reduce((result, key) => {
     result[key] = mapFunction(obj[key]);
     return result;
