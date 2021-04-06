c77568bb46009062b99f341e240cc3fd6efaa9ae
Dave Welsh <dave@qccareerschool.com>
2021-04-06 08:15:45 -0400

    Update for april promotions

--- src/transforms/defaultFreeCourseMap/getDefaultFreeCourseMap.ts
+++ src/transforms/defaultFreeCourseMap/getDefaultFreeCourseMap.ts
@@ -1,35 +1,56 @@
-import { isDesignCourse } from '../../courses';
+import { isDesignCourse, isEventFoundationCourse, isEventSpecialtyCourse } from '../../courses';
 import { CourseResult, MapFunction, PriceQueryOptions } from '../../types';
 
 export const getDefaultFreeCourseMap = (now: Date, options?: PriceQueryOptions): MapFunction<CourseResult, CourseResult> => {
   let freeDesignCount = 0;
+  let freeEventCount = 0;
 
   const student = options?.discountAll ?? false;
 
   return (courseResult: CourseResult, index: number, array: CourseResult[]): CourseResult => {
 
     // design -- buy any design course and get a second one free
-    if (options?.school === 'QC Design School' && !student) {
-      if (now.getTime() >= Date.UTC(2021, 2, 29, 13)) {
-        if (freeDesignCount < 1 && isDesignCourse(courseResult.code, [ 'VD' ]) && array.filter(c => isDesignCourse(c.code, [ 'VD' ])).length >= 2) {
-          freeDesignCount++;
-          return freeMap(courseResult);
-        }
-        if (courseResult.code === 'VD' && array.filter(c => isDesignCourse(c.code, [ 'VD' ])).length >= 1) {
-          return freeMap(courseResult);
+    if (options?.school === 'QC Design School') {
+      if (student) {
+        if (now.getTime() >= Date.UTC(2021, 3, 6, 13)) {
+          if (courseResult.code === 'VD' && array.some(c => isDesignCourse(c.code, [ 'VD' ]))) {
+            return freeMap(courseResult);
+          }
         }
       } else {
-        if (freeDesignCount < 1 && isDesignCourse(courseResult.code) && array.filter(c => isDesignCourse(c.code)).length >= 2) {
-          freeDesignCount++;
-          return freeMap(courseResult);
+        if (now.getTime() >= Date.UTC(2021, 3, 6, 13)) {
+          if (freeDesignCount < 1 && isDesignCourse(courseResult.code) && array.filter(c => isDesignCourse(c.code)).length >= 2) {
+            freeDesignCount++;
+            return freeMap(courseResult);
+          }
+        } else if (now.getTime() >= Date.UTC(2021, 2, 29, 13)) {
+          if (freeDesignCount < 1 && isDesignCourse(courseResult.code, [ 'VD' ]) && array.filter(c => isDesignCourse(c.code, [ 'VD' ])).length >= 2) {
+            freeDesignCount++;
+            return freeMap(courseResult);
+          }
+          if (courseResult.code === 'VD' && array.filter(c => isDesignCourse(c.code, [ 'VD' ])).length >= 1) {
+            return freeMap(courseResult);
+          }
+        } else {
+          if (freeDesignCount < 1 && isDesignCourse(courseResult.code) && array.filter(c => isDesignCourse(c.code)).length >= 2) {
+            freeDesignCount++;
+            return freeMap(courseResult);
+          }
         }
       }
     }
 
     // event -- buy EP and get LW and DW free
     if (options?.school === 'QC Event School' && !student) {
-      if ((courseResult.code === 'DW' || courseResult.code === 'LW') && array.some(c => c.code === 'EP')) {
-        return freeMap(courseResult);
+      if (now.getTime() >= Date.UTC(2021, 3, 6, 13)) {
+        if (freeEventCount < 1 && isEventSpecialtyCourse(courseResult.code) && array.some(c => isEventFoundationCourse(c.code))) {
+          freeEventCount++;
+          return freeMap(courseResult);
+        }
+      } else {
+        if ((courseResult.code === 'DW' || courseResult.code === 'LW') && array.some(c => c.code === 'EP')) {
+          return freeMap(courseResult);
+        }
       }
     }
 
