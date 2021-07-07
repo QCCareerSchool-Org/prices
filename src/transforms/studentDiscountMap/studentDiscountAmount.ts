823265c01a9368ef88a4b0aee32203c69da05114
Dave Welsh <dave@qccareerschool.com>
2021-07-07 12:06:39 -0400

    Revert back to £25 for student discount

--- src/transforms/studentDiscountMap/studentDiscountAmount.ts
+++ src/transforms/studentDiscountMap/studentDiscountAmount.ts
@@ -1,6 +1,6 @@
 import { CurrencyCode } from '../../types';
 
-const gbpDiscount = 50;
+const gbpDiscount = 25;
 const otherDiscount = 50;
 
 export const studentDiscountAmount = (currencyCode: CurrencyCode): number => (currencyCode === 'GBP' ? gbpDiscount : otherDiscount);
