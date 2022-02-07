66f59be00e675186c2f8b3791bd521e53e6f2e3e
Dave Welsh <dave@qccareerschool.com>
2022-02-07 10:54:31 -0500

    Fix typo in no-shipping message

--- src/noShippingMessage.ts
+++ src/noShippingMessage.ts
@@ -6,8 +6,8 @@ import { NoShipping } from './types';
 export const noShippingMessage = (noShipping: NoShipping, courses: string[], countryCode: string): string | undefined => {
   if (noShipping === 'REQUIRED') {
     const tel = telephoneNumber(countryCode);
-    return 'Due to international shipping restrictions, <strong>we do not ship</strong> kits or or bonus items' +
-      'to your country. The cost of your course' + (courses.length > 1 ? 's have ' : ' has ') +
+    return 'Due to international shipping restrictions, <strong>we do not ship</strong> kits or or bonus items to your country. ' +
+      'The cost of your course' + (courses.length > 1 ? 's have ' : ' has ') +
       'been reduced accordingly. You will have access to electronic course materials through the Online Student Center.' +
       (courses.some(c => isDesignCourse(c)) ? ' You will need to source your own design tools to complete your assignments. Please refer to your welcome email for more information.' : '') +
       (courses.some(c => isMakeupCourse(c)) ? ' You will have to source your own makeup and tools. Please refer to your course guide for the materials required for each assignment.' : '') +
