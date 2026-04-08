1a24353a9108cbada91b04b337f59ff6e883a6ef
Dave Welsh <dave@qccareerschool.com>
2026-04-08 11:00:01 -0400

    Change to just well-known crawlers

--- src/handlers/botHandler.ts
+++ src/handlers/botHandler.ts
@@ -6,13 +6,61 @@ import { logger } from '../logger';
 export const botHandler: RequestHandler = (req, res, next) => {
   res.setHeader('X-Robots-Tag', 'noindex');
 
-  const userAgent = req.headers['user-agent'];
+  if (req.method === 'OPTIONS') {
+    next();
+    return;
+  }
 
-  if (userAgent !== 'node' && isbot(userAgent)) {
-    logger.info('Blocked bot request', { userAgent, path: req.path, method: req.method });
+  const userAgent = req.headers['user-agent'];
+  if (!userAgent) {
+    next();
+    return;
+  }
+
+  if (knownCrawlers.some(regex => regex.test(userAgent))) {
+    logger.info('Blocked crawler', { userAgent, path: req.path, method: req.method });
 
     res.sendStatus(403);
     return;
   }
+
+  if (botExclusions.every(regex => !regex.test(userAgent)) && isbot(userAgent)) {
+    logger.info('Detected bot', { userAgent, path: req.path, method: req.method });
+  }
+
   next();
 };
+
+const botExclusions = [
+  /^node$/iu,
+  /^axios(?:\/[\d.]+)?$/iu,
+];
+
+const knownCrawlers: RegExp[] = [
+  // Major search engines
+  /googlebot/iu,
+  /bingbot/iu,
+  /slurp/iu, // Yahoo
+  /duckduckbot/iu,
+  /baiduspider/iu,
+  /yandex(bot)?/iu,
+
+  // Apple / misc legit crawlers
+  /applebot/iu,
+  /facebookexternalhit/iu,
+  /facebot/iu,
+  /twitterbot/iu,
+  /linkedinbot/iu,
+  /embedly/iu,
+
+  // SEO tools / common crawlers (optional depending on how aggressive you want to be)
+  /ahrefs(bot)?/iu,
+  /semrush(bot)?/iu,
+  /mj12bot/iu,
+  /dotbot/iu,
+  /seznambot/iu,
+  /sogou/iu,
+  /exabot/iu,
+  /petalbot/iu,
+  /bytespider/iu, // TikTok / ByteDance
+];
