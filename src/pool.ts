faa76f453430fb1cf74e9be690a320fdfcab4a02
Dave Welsh <dave@qccareerschool.com>
2026-06-29 17:05:27 -0400

    Added idleTimeout and maxIdle

--- src/pool.ts
+++ src/pool.ts
@@ -27,6 +27,8 @@ const options: PoolOptions = {
   password: process.env.DB_PASSWORD,
   user: process.env.DB_USERNAME,
   decimalNumbers: true,
+  idleTimeout: 5000, // five seconds as per https://vercel.com/kb/guide/connection-pooling-with-functions
+  maxIdle: 2,
 };
 
 if (typeof process.env.DB_SOCKET_PATH !== 'undefined') {
